b4754e06c6e855c38b51bc02a0487894a91f3848
Stephane Ndour <ndouradefemi@gmail.com>
2019-06-21 02:40:17 +0000

    on test gulp pour minifier le site

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var gzip = require('gulp-gzip');
 
 gulp.task('compress', function(done) {
-  gulp.src(['./dist/**/**/*.*'])
+  gulp.src(['./dist/**/*.*'])
       .pipe(gzip())
       .pipe(gulp.dest('./dist'));
       done();
