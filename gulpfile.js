cdc36b98ce2eec7e1a26ee9f0760ae9005bec9f1
Stephane Ndour <ndouradefemi@gmail.com>
2019-06-21 02:28:07 +0000

    on test gulp pour minifier le site

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,9 @@
 var gulp = require('gulp');
 var gzip = require('gulp-gzip');
 
-gulp.task('compress', function() {
+gulp.task('compress', function(done) {
   gulp.src(['./dist/**/**/*.*'])
       .pipe(gzip())
       .pipe(gulp.dest('./dist'));
+      done();
 });
